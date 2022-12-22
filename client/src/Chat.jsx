53fdcf995cec19674226e16736c1a59693ed832a
Ali Maher <ali.maher.mohammed@gmail.com>
2022-12-22 08:34:49 +0200

    Post Message Mutation

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   useQuery,
   gql,
+  useMutation,
 } from "@apollo/client";
-import { Box, Container, Flex, Text } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Grid,
+  Text,
+  TextInput,
+} from "@mantine/core";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
@@ -20,6 +29,12 @@ const GET_MESSAGES = gql`
       id
       user
     }
+  }
+`;
+
+const POST_MESSAGE = gql`
+  mutation ($user: String!, $content: String!) {
+    postMessage(user: $user, content: $content)
   }
 `;
 
@@ -71,9 +86,64 @@ const Messages = ({ user }) => {
 };
 
 const Chat = () => {
+  const [state, setState] = useState({
+    user: "Ali",
+    content: "",
+  });
+
+  const [postMessage] = useMutation(POST_MESSAGE);
+
+  const onSend = () => {
+    if (state.content.length > 0) {
+      postMessage({
+        variables: state,
+      });
+    }
+    setState({
+      ...state,
+      content: "",
+    });
+  };
+
   return (
     <Container>
-      <Messages user='0' />
+      <Messages user={state.user} />
+      <Grid justify='center' align='end'>
+        <Grid.Col span={2} sx={{ padding: 0 }}>
+          <TextInput
+            label='User'
+            value={state.user}
+            onChange={(e) =>
+              setState({
+                ...state,
+                user: e.target.value,
+              })
+            }
+          />
+        </Grid.Col>
+        <Grid.Col span='auto' px='sm' py='0'>
+          <TextInput
+            autocomplete='off'
+            label='Content'
+            placeholder='Your Message!'
+            value={state.content}
+            onChange={(e) =>
+              setState({
+                ...state,
+                content: e.target.value,
+              })
+            }
+            onKeyUp={(e) => {
+              if (e.keyCode === 13) {
+                onSend();
+              }
+            }}
+          />
+        </Grid.Col>
+        <Grid.Col span={1} sx={{ padding: 0 }}>
+          <Button onClick={() => onSend()}>Send</Button>
+        </Grid.Col>
+      </Grid>
     </Container>
   );
 };
